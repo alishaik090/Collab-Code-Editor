9025c11d5d8508137043da3cc6d4cf1d45152645
alishaik090 <skmdali546@gmail.com>
2026-04-21 09:42:56 +0530

    Fix socket timeouts

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,3 +1,10 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://collab-code-editor-1-3f3i.onrender.com");
+export const socket = io("https://collab-code-editor-1-3f3i.onrender.com", {
+    reconnection: true,
+    reconnectionDelay: 1000,
+    reconnectionDelayMax: 5000,
+    reconnectionAttempts: Infinity,
+    timeout: 60000,
+    transports: ["websocket", "polling"]
+});
